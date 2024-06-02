b5f153d5349b663554f9b6892fcfba3cfc45962c
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2024-06-02 09:18:12 +0800

    push

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'cio_db',
+        'database'     => 'city_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
